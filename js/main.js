6cd06e2a540d5702277fb1170f4c78e62a460c2f
Javier <javiafellay96@gmail.com>
2020-10-13 20:23:19 -0300

    Añadiendo Plugins de CountDown y Contador

--- js/main.js
+++ js/main.js
@@ -168,4 +168,17 @@
 
 		return false;
 	});
+
+	//Animaciónes para los numeros
+	$('.resumen-evento li:nth-child(1) p').animateNumber({ number: 6}, 1300);
+	$('.resumen-evento li:nth-child(2) p').animateNumber({ number: 15}, 1300);
+	$('.resumen-evento li:nth-child(3) p').animateNumber({ number: 3}, 2000);
+	$('.resumen-evento li:nth-child(4) p').animateNumber({ number: 9}, 1600);
+
+	$('.cuenta-regresiva').countdown('2020/12/31 09:00:00', function(event){
+		$('#dias').html(event.strftime('%D'));
+		$('#horas').html(event.strftime('%H'));
+		$('#minutos').html(event.strftime('%M'));
+		$('#segundos').html(event.strftime('%S'));
+	});
 });
