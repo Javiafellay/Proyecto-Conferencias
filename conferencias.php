cf36a205d40fe40abd05b7a971976034e4de9873
Javier <javiafellay96@gmail.com>
2020-10-24 22:11:55 -0300

    Haciendo el sitio Dinámico

---
+++ conferencias.php
@@ -0,0 +1,54 @@
+<?php include_once 'includes/templates/header.php'; ?>
+
+<section class="seccion contenedor">
+  <h2>La Mejor conferencia de Diseño Web</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Quo autem ratione exercitationem aspernatur Quo autem ratione exercitationem aspernatur Quo autem ratione exercitationem aspernatur Quo autem ratione exercitationem aspernatur Quo autem ratione exercitationem aspernatur </p>
+</section><!--seccion-->
+
+<section class="seccion contenedor">
+
+  <h2>Galeria de Fotos</h2>
+
+  <div class="galeria">
+    <a href="img/galeria/01.jpg" data-lightbox="galeria">
+      <img src="img/galeria/thumbs/01.jpg" alt="">
+    </a>
+     <a href="img/galeria/02.jpg" data-lightbox="galeria">
+      <img src="img/galeria/thumbs/02.jpg" alt="">
+    </a>
+     <a href="img/galeria/03.jpg" data-lightbox="galeria">
+      <img src="img/galeria/thumbs/03.jpg" alt="">
+    </a>
+     <a href="img/galeria/04.jpg" data-lightbox="galeria">
+      <img src="img/galeria/thumbs/04.jpg" alt="">
+    </a>
+     <a href="img/galeria/05.jpg" data-lightbox="galeria">
+      <img src="img/galeria/thumbs/05.jpg" alt="">
+    </a>
+     <a href="img/galeria/06.jpg" data-lightbox="galeria">
+      <img src="img/galeria/thumbs/06.jpg" alt="">
+    </a>
+     <a href="img/galeria/07.jpg" data-lightbox="galeria">
+      <img src="img/galeria/thumbs/07.jpg" alt="">
+    </a>
+     <a href="img/galeria/08.jpg" data-lightbox="galeria">
+      <img src="img/galeria/thumbs/08.jpg" alt="">
+    </a>
+     <a href="img/galeria/09.jpg" data-lightbox="galeria">
+      <img src="img/galeria/thumbs/09.jpg" alt="">
+    </a>
+     <a href="img/galeria/10.jpg" data-lightbox="galeria">
+      <img src="img/galeria/thumbs/10.jpg" alt="">
+    </a>
+         <a href="img/galeria/11.jpg" data-lightbox="galeria">
+      <img src="img/galeria/thumbs/11.jpg" alt="">
+    </a>
+         <a href="img/galeria/12.jpg" data-lightbox="galeria">
+      <img src="img/galeria/thumbs/12.jpg" alt="">
+    </a>
+  </div>
+
+</section>
+
+<?php include_once 'includes/templates/footer.php';  ?>
---
+++ includes/templates/footer.php
@@ -0,0 +1,56 @@
+<footer class="site-footer">
+  <div class="contenedor clearfix">
+    <div class="footer-informacion">
+      <h3>Sobre<span>Margua</span></h3>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque incidunt, dolorum nobis! Magnam dignissimos quas.</p>
+    </div><!--footer-Informacion-->
+
+    <div class="ultimos-tweets">
+      <h3>Ultimos<span>Tweets</span></h3>
+      <ul>
+        <li>Officia fugiat maxime hic at cupiditate explicabo quidem obcaecati, #deserunt velit quod, numquam!</li>
+        <li>Officia fugiat maxime hic at cupiditate explicabo quidem obcaecati, #deserunt velit quod, numquam!</li>
+        <li>Officia fugiat maxime hic at cupiditate explicabo quidem obcaecati, #deserunt velit quod, numquam!</li>
+      </ul>
+    </div><!--ultimos-tweets-->
+
+    <div class="menu">
+      <h3>Redes<span>Sociales</span></h3>
+            <nav class="redes-sociales">
+        <a href="#"><i class="fab fa-facebook"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-whatsapp"></i></a>
+        <a href="#"><i class="fab fa-youtube"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+      </nav>
+    </div><!--Menu-->
+
+</div><!--Contenedor-->
+
+<p class="copyright">
+  Todos los Derechos Reservados Margua 2020
+</p>
+  
+</footer>
+
+  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+  <script src="js/jquery-1.12.0.min.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/lightbox.js"></script>
+  <script src="js/jquery.lettering.js"></script>
+  
+  <script src="js/main.js"></script>
+
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+
+</html>
